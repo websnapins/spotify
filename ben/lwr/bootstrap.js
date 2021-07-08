24629f0a0d070cc31516b0af400d0624fdfd9355
websnapins <anal.shah@salesforce.com>
2021-07-07 17:05:05 -0700

    Update bootstrap.js

--- ben/lwr/bootstrap.js
+++ ben/lwr/bootstrap.js
@@ -721,8 +721,8 @@
 
 //			iframe.src = getSiteUrl() + "/embeddedService/embeddedService.app";
 //			iframe.src = "https://eswmessagingchat-175482fb00f-175d82b2ab2.stmfa.stm.force.com/lwcbyo1/"
-//			iframe.src = "https://eswmessagingchat-175482fb00f-175d82b2ab2.stmfa.stm.force.com/byolwr3";
-			iframe.src = "https://benco11.my.localhost.sfdcdev.site.com:6101/byolwr4"
+			iframe.src = "https://eswmessagingchat-175482fb00f-175d82b2ab2.stmfa.stm.force.com/byolwr3";
+//			iframe.src = "https://benco11.my.localhost.sfdcdev.site.com:6101/byolwr4"
 
 			iframe.title = "Chat with an Agent";
 			iframe.className = IFRAME_NAME;
